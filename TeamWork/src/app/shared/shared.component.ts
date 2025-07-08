f53e0de22d5a7889484127642bcb62026bbae816
DipeshNawani <dipeshnawani1610@gmail.com>
2025-07-08 14:41:16 +0530

    sharedModuleAdded

---
+++ TeamWork/src/app/shared/shared.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared',
+  templateUrl: './shared.component.html',
+  styleUrls: ['./shared.component.css']
+})
+export class SharedComponent {
+  
+}
+
